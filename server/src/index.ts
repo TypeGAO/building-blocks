e72e7d97ffb9e9d38a3410cf6b991d6d2e1ca6ad
Liberato Aguilar <liberatoalanaguilar@gmail.com>
2023-09-27 09:12:45 -0500

    Installed backend packages for db, routing, env,  also added .gitignore

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,25 @@
-import express from 'express';
+import { Request, Response } from 'express';
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = require('./routes/router.ts');
+ 
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-app.get('/', (req, res) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+process.on('SIGINT', function() {
+    console.log('Server Successfully Shut down');
+    process.exit(0);
+});
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Building Blocks Server!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+router(app);
---
+++ server/src/routes/router.ts
@@ -0,0 +1,8 @@
+import { Application } from 'express';
+//const users = require('./user.js');
+
+const mountRoutes = (app: Application) => {
+    //app.use('/users', users);
+};
+     
+module.exports = mountRoutes;
