c92f4ffcf9f528b150a88996f503adb612f0b67b
Brennan Rouhana <8935419+Brouhana@users.noreply.github.com>
2023-09-27 10:14:29 -0500

    add button

---
+++ client/src/components/button/Button.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, StoryObj } from "@storybook/react"
+import { action } from "@storybook/addon-actions"
+import Button from "./Button"
+
+import "../../index.css"
+import "../../styles/colors.css"
+import "../../styles/spacing.css"
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  title: "Button",
+  argTypes: {
+    color: {
+      options: ["green", "blue", "neutral"],
+      control: { type: "radio" },
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<typeof Button>
+
+export const Default: Story = {
+  args: {
+    color: "green",
+    children: "Run",
+    disabled: false,
+    onClick: action("onClick"),
+  },
+}
