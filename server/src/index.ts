41596bdd03c4e3dd7f1b008e56eb0da0ed1e3599
Liberato Aguilar <liberatoalanaguilar@gmail.com>
2023-09-27 09:55:19 -0500

    Basic Socket Implementation

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from 'express';
+import { Server, Socket } from 'socket.io';
 
+const { createServer } = require('node:http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./routes/router.ts');
  
 const app = express();
 const port = 3000;
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,10 +20,32 @@ process.on('SIGINT', function() {
 });
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Building Blocks Server!');
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <title>Test</title>
+      </head>
+      <script src="/socket.io/socket.io.js"></script>
+      <script>
+        const socket = io();
+      </script>
+      <body>
+      </body>
+    </html>
+  `);
 });
 
-app.listen(port, () => {
+io.on('connection', (socket: Socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
+
 router(app);
