4cb1cd1986c0e80f757e5c4663b4373b20b6363a
Brennan Rouhana <8935419+Brouhana@users.noreply.github.com>
2023-10-03 15:28:10 -0500

    add lg prop size

--- client/src/components/button/Button.tsx
+++ client/src/components/button/Button.tsx
@@ -2,16 +2,24 @@ import styles from "./Button.module.css"
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   color?: "neutral" | "green" | "blue"
+  size?: "md" | "lg"
   disabled?: boolean
   onClick: (e: React.MouseEvent<HTMLElement>) => void
   children: string
 }
 
-function Button({ color, disabled, onClick, children, ...props }: ButtonProps) {
+function Button({
+  color,
+  size,
+  disabled,
+  onClick,
+  children,
+  ...props
+}: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className={styles[color || "neutral"]}
+      className={`${styles[color || "neutral"]} ${styles[size || "md"]}`}
       disabled={disabled || false}
       {...props}
     >
