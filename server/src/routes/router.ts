8a4c906e61981108196acafe20ad0a28b631fb05
Liberato Aguilar <liberatoalanaguilar@gmail.com>
2023-10-06 14:15:46 -0500

    Database connection, question set and question CRUD api

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,1 +1,11 @@
-// TODO: Add routes for REST API
+import { Express } from "express";
+
+const question_sets = require('./question_set_api.ts');
+const questions= require('./questions_api.ts');
+
+const mountRoutes = (app: Express) => {
+    app.use('/questionSets', question_sets);
+    app.use('/questions', questions);
+};
+     
+module.exports = mountRoutes;
