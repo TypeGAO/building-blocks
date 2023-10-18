e82bcff7090efc3b610e1ee87d23fd2d02b7ee26
Andrew-Wheir <andywheir@tamu.edu>
2023-10-17 20:07:20 -0500

    Initial Commit

---
+++ client/src/pages/CreateGame.tsx
@@ -0,0 +1,16 @@
+
+import { QuestionBuilder } from "../features/create-game"
+
+
+function QuizCreation() {
+  return (
+    <div className="QuizCreation">
+      <QuestionBuilder />
+    </div>
+  );
+}
+
+export default QuizCreation;
+
+
+
