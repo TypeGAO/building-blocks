12b7bbf6470036f79540472462383d5fd237ef5e
Wheirdo <aswheir@gmail.com>
2023-10-25 09:36:14 -0500

    fix import

--- client/src/features/create-game/QuestionBuilder.tsx
+++ client/src/features/create-game/QuestionBuilder.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Button, Input } from "../../../components"
+import { Button, Input } from "../../components"
 
 function QuestionBuilder() {
   const [questions, setQuestions] = useState([{
