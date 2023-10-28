5101a4ac7b87fa95d70395da50e9ee90fa984849
Brennan Rouhana <8935419+Brouhana@users.noreply.github.com>
2023-10-27 22:20:35 -0500

    GameHeader changes

--- client/src/pages/PlayerGame.tsx
+++ client/src/pages/PlayerGame.tsx
@@ -4,36 +4,40 @@ import styles from "./PlayerGame.module.css"
 import useGameActivity from "../hooks/useGameActivity"
 import { Player } from "../types"
 import { Code } from "../features/code"
+import { GameHeader } from "../components"
 
 function PlayerGame() {
   const { gameActivity } = useGameActivity()
 
   return (
-    <div className={styles.container}>
-      <div className={styles.col}>question col</div>
-      <div className={styles.col}>
-        <Code />
-        <h2>
-          Question:{" "}
-          {gameActivity.players.find(
-            (player: Player) =>
-              player.nickname === gameActivity.nickname &&
-              player.roomId === gameActivity.roomId
-          )?.currentQuestion ?? 0}
-        </h2>
-        <h2>
-          Score:{" "}
-          {gameActivity.players.find(
-            (player: Player) =>
-              player.nickname === gameActivity.nickname &&
-              player.roomId === gameActivity.roomId
-          )?.score ?? 0}
-        </h2>
+    <>
+      <GameHeader />
+      <div className={styles.container}>
+        <div className={styles.col}>question col</div>
+        <div className={styles.col}>
+          <Code />
+          <h2>
+            Question:{" "}
+            {gameActivity.players.find(
+              (player: Player) =>
+                player.nickname === gameActivity.nickname &&
+                player.roomId === gameActivity.roomId
+            )?.currentQuestion ?? 0}
+          </h2>
+          <h2>
+            Score:{" "}
+            {gameActivity.players.find(
+              (player: Player) =>
+                player.nickname === gameActivity.nickname &&
+                player.roomId === gameActivity.roomId
+            )?.score ?? 0}
+          </h2>
+        </div>
+        <div className={styles.col}>
+          <Scene />
+        </div>
       </div>
-      <div className={styles.col}>
-        <Scene />
-      </div>
-    </div>
+    </>
   )
 }
 
