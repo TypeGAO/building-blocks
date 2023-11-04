fcfb3cf0d242305e6fdae11dae7536b1ba24c5d0
Ardian <ardian.kuswanto@gmail.com>
2023-11-03 21:05:11 -0500

    Adds framework for testing services on serverside

---
+++ server/tests/Services.spec.ts
@@ -0,0 +1,26 @@
+import { describe } from "node:test";
+import { addPlayer, generateUniqueCode, newGameActivity } from "../services/generateGameService";
+var expect = require('chai').expect;
+
+describe('Test /generateManagerService', () => {
+    describe('generateUniqueCode() Test', () => {
+        it('Should be 7 characters long', () => {
+            const codeString = generateUniqueCode();
+            expect(codeString).to.have.lengthOf(7);
+        });
+    });
+
+    describe('addPlayer() Test', () => {
+        it('Should return currentQuestion as 0', () => {
+            const newPlayer = addPlayer("testid", "test");
+            expect(newPlayer.currentQuestion).to.equal(0);
+        });
+    });
+
+    describe('newGameActivity()', () => {
+        it('Should return time as -1', () => {
+            const newGame = newGameActivity("testSocket", "testId");
+            expect(newGame.time).to.equal(-1);
+        });
+    });
+});
