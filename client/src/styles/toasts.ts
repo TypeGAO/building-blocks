d74cb1d80befcca8aec752ed754f6d84dc18233b
Brennan Rouhana <8935419+Brouhana@users.noreply.github.com>
2023-11-05 00:25:45 -0500

    quick add: minor changes + add success style

--- client/src/styles/toasts.ts
+++ client/src/styles/toasts.ts
@@ -5,13 +5,26 @@ export const toastStyles = {
     color: "var(--neutral-50)",
     fontSize: "var(--20)",
     borderRadius: "var(--8)",
+    padding: "var(--12)",
     fontWeight: 600,
+  },
+  success: {
+    style: {
+      background: "var(--green-50)",
+      border: "var(--4) solid var(--green-500)",
+      color: "var(--green-500)",
+      boxShadow: "0px 8px 0px 0px rgba(255, 255, 255, 0.50) inset",
+    },
+    iconTheme: {
+      primary: "var(--green-400)",
+      secondary: "var(--green-50)",
+    },
+    duration: 3000,
   },
   error: {
     style: {
       background: "var(--red-50)",
       border: "var(--4) solid var(--red-500)",
-      padding: "var(--16)",
       color: "var(--red-500)",
       boxShadow: "0px 8px 0px 0px rgba(255, 255, 255, 0.50) inset",
     },
