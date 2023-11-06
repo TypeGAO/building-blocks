eb4eccb69d4bcdbac60a3ef9425f3c95f604ad4c
Brennan Rouhana <8935419+Brouhana@users.noreply.github.com>
2023-11-06 08:15:18 -0600

    add base - svgs, base block and crane

--- client/src/features/scene/Scene.tsx
+++ client/src/features/scene/Scene.tsx
@@ -1,6 +1,22 @@
+/// <reference types="vite-plugin-svgr/client" />
+import Block_1 from "../../assets/blocks/block_1.svg?react"
+import Crane from "../../assets/crane.svg?react"
+import useGameActivity from "../../hooks/useGameActivity"
+import styles from "./styles.module.css"
+
 function Scene() {
+  const { currentPlayer } = useGameActivity()
+
+  console.log(currentPlayer.buildingBlocksId)
   return (
-    <div style={{ backgroundColor: "var(--blue-200)", height: "100%" }}></div>
+    <div className={styles.scene}>
+      <div className={styles.crane}>
+        <Crane height="350px" />
+      </div>
+      <div className={styles.tower}>
+        <Block_1 />
+      </div>
+    </div>
   )
 }
 
