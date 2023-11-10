88a3642198d313c6a47dffa4d4dc4211d49bc9ff
Brennan Rouhana <8935419+Brouhana@users.noreply.github.com>
2023-11-09 18:03:14 -0600

    refactor question set + add styling

---
+++ client/src/features/create-game/SelectGameTime.tsx
@@ -0,0 +1,66 @@
+import { useState } from "react"
+import useGameActivity from "../../hooks/useGameActivity"
+import { socket } from "../../socket"
+import { Button } from "../../components"
+import styles from "./styles.module.css"
+
+interface SelectGameTimeProps {
+  setStep: React.Dispatch<React.SetStateAction<number>>
+}
+
+const timeOptions = [
+  {
+    name: "5 min",
+    seconds: 300,
+  },
+  {
+    name: "10 min",
+    seconds: 600,
+  },
+  {
+    name: "15 min",
+    seconds: 900,
+  },
+  {
+    name: "20 min",
+    seconds: 1200,
+  },
+  {
+    name: "25 min",
+    seconds: 1500,
+  },
+  {
+    name: "30 min",
+    seconds: 1800,
+  },
+  {
+    name: "45 min",
+    seconds: 2700,
+  },
+]
+
+function SelectGameTime({ setStep }: SelectGameTimeProps) {
+  const [time, setTime] = useState(600)
+  const { gameActivity } = useGameActivity()
+
+  const handleClick = (seconds: number) => {
+    setTime(seconds)
+    socket.emit("setTime", gameActivity.roomId, time)
+    setStep(1)
+  }
+
+  return (
+    <div className={styles.timeContainer}>
+      {timeOptions.map((option) => (
+        <Button
+          key={option.seconds}
+          onClick={() => handleClick(option.seconds)}
+        >
+          {option.name}
+        </Button>
+      ))}
+    </div>
+  )
+}
+
+export default SelectGameTime
