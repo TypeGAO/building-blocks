2384d5b4c2b9b8a374fedd11208668f25600618b
Brennan Rouhana <8935419+Brouhana@users.noreply.github.com>
2023-11-12 22:06:16 -0600

    quick add: logo

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,4 +1,5 @@
 import styles from "./Header.module.css"
+import Logo from "../../assets/logo.svg?react"
 
 interface HeaderProps {
   rightElement?: JSX.Element
@@ -9,7 +10,7 @@ function Header({ rightElement, leftElement }: HeaderProps) {
   return (
     <div className={styles.header}>
       <div className={styles.left}>{leftElement}</div>
-      <div className={styles.mid}></div>
+      <div className={styles.mid}><Logo /></div>
       <div className={styles.right}>{rightElement}</div>
     </div>
   )
