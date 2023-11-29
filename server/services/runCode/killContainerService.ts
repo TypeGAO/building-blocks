83bd947100bc84ccddb0eb44ddb45271b6e835f0
RDN <91217050+TypeGAO@users.noreply.github.com>
2023-11-28 21:41:56 -0600

    Delete server/services/runCode directory

--- server/services/runCode/killContainerService.ts
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { spawn, spawnSync, execSync, exec } = require('child_process');
-
-export function killContainer(dockerName: string) {
-    //Close container
-    execSync('docker container kill ' + dockerName);
-}
